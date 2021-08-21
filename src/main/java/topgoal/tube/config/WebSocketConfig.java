ee013321cd768dc0f3de8acb04848a8630678aea
Jung Ju Hee <jieboong82@daum.net>
2021-08-22 02:53:29 +0900

    웹소켓 build 관련 설정 변경

--- src/main/java/topgoal/tube/config/WebSocketConfig.java
+++ src/main/java/topgoal/tube/config/WebSocketConfig.java
@@ -1,23 +1,36 @@
 package topgoal.tube.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-@EnableWebSocketMessageBroker
+@Profile("stomp")
 @Configuration
+@EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
-    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chat");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/talk")
+                .withSockJS();
+        log.info("{}", "세션 접속");
+
+    }
+
 }
--- src/main/java/topgoal/tube/controller/ChatController.java
+++ src/main/java/topgoal/tube/controller/ChatController.java
@@ -2,25 +2,29 @@ package topgoal.tube.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import topgoal.tube.entity.ChatMessage;
 import topgoal.tube.service.MessageService;
 
-
+@Profile("stomp")
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
 
-    @Autowired
-    private SimpMessagingTemplate template;
-
-    @Autowired
+    private final SimpMessagingTemplate template;
     private final MessageService messageService;
 
-    @MessageMapping("/chat/message")
-    public void message (ChatMessage message) throws Exception{
+    @MessageMapping("/join")
+    public void join(ChatMessage message) {
+        message.setMessage(message.getUserId().getName() + "님이 입장하셨습니다.");
+        template.convertAndSend("/chat/"+message.getRoomId(), message);
+    }
+
+    @MessageMapping("/message")
+    public void message(ChatMessage message) throws Exception {
         messageService.setMessage(message);
         template.convertAndSend("/chat/" + message.getRoomId(), message);
     }
